df4a7ad32e4e4bde6ecf49a8a813bc07495f7afb
HackingAll <87182741+HackingAllYT@users.noreply.github.com>
2023-03-31 21:03:27 +0200

    Open Kodi when hdmi is connected
    TODO: stop kodi when hdmi is disconnected

--- openKodi.py
+++ openKodi.py
@@ -1,14 +1,26 @@
 import pyudev
+from multiprocessing import Process
+import os
 
 WATCH_FILE = '/sys/class/drm/card0-HDMI-A-1/edid'
 WAIT_TIME  = 5
+
+PROC_PID = -1
 
 context = pyudev.Context()
 monitor = pyudev.Monitor.from_netlink(context)
 monitor.filter_by('drm')
 device = pyudev.Devices.from_path(context, '/sys/class/drm/card0-HDMI-A-1')
+# device = pyudev.Devices.from_path(context, '/sys/class/drm/card0-HDMI-A-1')[0]
+
+
+
+def open_kodi():
+    os.popen('kodi')
+
 
 def handle_event(observer, device):
+    global PROC_PID
     #if device.sys_path == '/sys/class/drm/card0-HDMI-A-1/edid':
     print('El archivo /sys/class/drm/card0-HDMI-A-1/edid ha sido modificado')
     # Leer el contenido del archivo
@@ -17,8 +29,22 @@ def handle_event(observer, device):
         # print(content)
         if content:
             print ("ENCENDIDO")
+            if PROC_PID == -1:
+                print ('Imos inicialo')
+                p = Process(target=open_kodi)
+                p.start()
+                PROC_PID = p.pid
+                print ('INICIADO')
         else:
             print ("APAGADO")
+            if PROC_PID != -1:
+                os.popen(['kill', PROC_PID])
+            PROC_PID = -1
+            print ('KILL FEITO')
+
+with open(WATCH_FILE, "rb") as f:
+        content = f.read()
+        print ('ESTADO ACTUAL:', 'encendido' if content else 'apagado')
 
 observer = pyudev.MonitorObserver(monitor, handle_event)
 observer.start()
